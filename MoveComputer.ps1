3623e36f5b851326b0e7a771f4f43229c8f1bec1
TheHalestormHunter <thehalestormhunter@protonmail.com>
2024-02-12 22:40:35 -0600

    Update MoveComputer.ps1

--- MoveComputer.ps1
+++ MoveComputer.ps1
@@ -25,4 +25,4 @@ $comp = Read-Host -Prompt "Enter Computer Name"
 
 
 
-Move-ADObject -Identity:"CN=$comp,OU=Computers,DC=bhmanagement,DC=com" -TargetPath:"OU=BHCO,OU=Computers,OU=Corp,DC=bhmanagement,DC=com"
+Move-ADObject -Identity:"CN=$comp,OU=Laptops,OU=_Windows 10,OU=Physical Workstations,OU=Machines,DC=int,DC=Contoso,DC=com" -TargetPath:"OU=Travel Directors,OU=Laptops,OU=_Windows 10,OU=Physical Workstations,OU=Machines,DC=int,DC=Contoso,DC=com"
